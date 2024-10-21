e403f9cd590fea11aa6d56446f8a426cc6f17111
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-10-21 12:42:41 +0200

    e05

--- øvelser/e05_two_dice/src/two_dice.py
+++ øvelser/e05_two_dice/src/two_dice.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def main():
+
+    diceOne = range(1, 7)
+    diceTwo = range(1, 7)
+
+    for i in diceOne:
+        for j in diceTwo:
+            if i + j == 5:
+                print((i, j))
+
     pass
+
 
 if __name__ == "__main__":
     main()
