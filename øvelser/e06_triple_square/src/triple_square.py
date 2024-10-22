3c53106a31abe58ac044554f8ba4e8729bbbd39d
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-10-22 09:33:21 +0200

    e06

--- øvelser/e06_triple_square/src/triple_square.py
+++ øvelser/e06_triple_square/src/triple_square.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
 
+def triple(number):
+    return number * 3
+
+
+def square(number):
+    return number ** 2
+
+
 def main():
-    pass
+    for number in range(1, 11):
+        tr = triple(number)
+        sq = square(number)
+
+        if (sq > tr):
+            break
+        print(f"triple({number})=={tr} square({number})=={sq}")
+
 
 if __name__ == "__main__":
     main()
