d1b81b0db84ac619d14b2ede17d3094abc8093a6
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-10-22 13:51:57 +0200

    e13

--- øvelser/e13_reverse_dictionary/src/reverse_dictionary.py
+++ øvelser/e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    return {}
+
+    rd = {}
+
+    for key, value in d.items():
+        for i in value:
+            if i in rd:
+                rd[i].append(key)
+            else:
+                rd[i] = [key]
+
+    return rd
+
 
 def main():
-    pass
+
+    d = {'move': ['liikuttaa'], 'hide': ['piilottaa',
+                                         'salata'], 'six': ['kuusi'], 'fir': ['kuusi']}
+    print(reverse_dictionary(d))
+
 
 if __name__ == "__main__":
     main()
