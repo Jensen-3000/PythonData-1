7b38cf17fa2d0736ed96a637ddd6547fcba06a08
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-10-22 13:00:54 +0200

    e11

--- øvelser/e11_interleave/src/interleave.py
+++ øvelser/e11_interleave/src/interleave.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 def interleave(*lists):
-    return []
+
+    lst = []
+    for i in zip(*lists):
+        lst.extend(i)
+
+    return lst
+
 
 def main():
     print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
+
 
 if __name__ == "__main__":
     main()
