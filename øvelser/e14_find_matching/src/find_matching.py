902afbdbf8647f018c6d92cfc6a6cea7c27227a6
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-10-22 14:02:27 +0200

    e14

--- øvelser/e14_find_matching/src/find_matching.py
+++ øvelser/e14_find_matching/src/find_matching.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    return []
+
+    lst = []
+
+    for i, w in enumerate(L):
+        if pattern in w:
+            lst.append(i)
+    return lst
+
 
 def main():
-    pass
+
+    print(find_matching(["sensitive", "engine", "rubbish", "comment"], "en"))
+
 
 if __name__ == "__main__":
     main()
