b208d56ccb952e8b82fa511fb743e73857ec7605
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-10-22 13:32:17 +0200

    e12

--- øvelser/e12_distinct_characters/src/distinct_characters.py
+++ øvelser/e12_distinct_characters/src/distinct_characters.py
@@ -2,12 +2,16 @@
 
 def distinct_characters(L):
     d = {}
-    for w in L:
-        d[w] = len(set(w))
+
+    for word in L:
+        d[word] = len(set(word))
+
     return d
+
 
 def main():
     print(distinct_characters(["check", "look", "try", "pop"]))
+
 
 if __name__ == "__main__":
     main()
