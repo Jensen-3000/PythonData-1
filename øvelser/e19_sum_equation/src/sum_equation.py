c1efb0cd05468dd6979ef5978198c1dd3bdb63ce
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-10-23 10:11:55 +0200

    e19

--- øvelser/e19_sum_equation/src/sum_equation.py
+++ øvelser/e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
+
 def sum_equation(L):
-    return ""
+    # if not L:
+    #     return "0 = 0"
+
+    # s = [str(i) for i in L]
+    # s2 = f"{' + '.join(s)} = {sum(L)}"
+    # return s2
+
+    # OneLiner er street
+    return f"{' + '.join([str(i) for i in L])} = {sum(L)}" if L else "0 = 0"
+
 
 def main():
-    pass
+    print(sum_equation([1, 5, 7]))
+    print(sum_equation([]))
+
 
 if __name__ == "__main__":
     main()
