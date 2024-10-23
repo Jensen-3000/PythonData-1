b89c2b19e4c89735b59e5bd29a793c5f3bc04f1a
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-10-23 13:06:40 +0200

    e28

--- øvelser/e28_prepend/src/prepend.py
+++ øvelser/e28_prepend/src/prepend.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
+
 class Prepend(object):
     # Add the methods of the class here
 
+    def __init__(self, s):
+        self.start = s
+
+    def write(self, str):
+        print(self.start + str)
+
+
 def main():
-    pass
+    p = Prepend("+++ ")
+    p.write("Hello")
+
 
 if __name__ == "__main__":
     main()
