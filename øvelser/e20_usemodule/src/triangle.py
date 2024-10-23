0e1486ceecf4667a6936d8bd695915aaf06e2dbb
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-10-23 10:50:10 +0200

    e20

--- øvelser/e20_usemodule/src/triangle.py
+++ øvelser/e20_usemodule/src/triangle.py
@@ -1,1 +1,17 @@
 # Enter you module contents here
+"""Triangles are evil and should be avoided at all costs."""
+
+from math import sqrt
+
+__version__ = "v9000"
+__author__ = "Mr. Big Handsome"
+
+
+def hypothenuse(a, b):
+    """I DONT LIKE MATH"""
+    return sqrt(a**2 + b**2)
+
+
+def area(a, b):
+    """I DONT LIKE MATH"""
+    return 0.5 * a * b
--- øvelser/e20_usemodule/src/usemodule.py
+++ øvelser/e20_usemodule/src/usemodule.py
@@ -6,8 +6,12 @@ try:
 except ModuleNotFoundError:
     import triangle
 
+
 def main():
     # Call the functions from here
+    print(triangle.area(3, 4))
+    print(triangle.hypothenuse(3, 4))
+
 
 if __name__ == "__main__":
     main()
