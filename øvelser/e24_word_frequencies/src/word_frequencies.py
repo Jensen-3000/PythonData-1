a6946287e4eafef3878de11df359c80d43bcdae9
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-10-23 12:47:00 +0200

    e24

--- øvelser/e24_word_frequencies/src/word_frequencies.py
+++ øvelser/e24_word_frequencies/src/word_frequencies.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python3
 
+
 def word_frequencies(filename):
-    return {}
+    dict = {}
+
+    with open(filename, "r") as file:
+        for line in file:
+            for word in line.split():
+                word = word.strip("""!"#$%&'()*,-./:;?@[]_""")
+                if word in dict:
+                    dict[word] += 1
+                else:
+                    dict[word] = 1
+
+    return dict
+
+
+def get_to_5_p_word_frequencies(filename):
+    wd = word_frequencies(filename)
+    wd = dict(sorted(wd.items(), key=lambda x: x[1], reverse=True))
+
+    for w, c in list(wd.items())[:5]:
+        print(w, c, sep="\t")
+
 
 def main():
-    pass
+    fname = r"øvelser\e24_word_frequencies\src\alice.txt"
+    # print(word_frequencies(fname))
+
+    get_to_5_p_word_frequencies(fname)
+
 
 if __name__ == "__main__":
     main()
